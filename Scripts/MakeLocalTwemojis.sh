0a55bcfa60a27633e9a75cfacfc450c935d24a36
octospacc <octo@tutamail.com>
2022-06-10 19:43:13 +0200

    Data di creazione e modifica nelle pagine dei post

--- Scripts/MakeLocalTwemojis.sh
+++ Scripts/MakeLocalTwemojis.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+File=twemoji-amazing-local.css
+
 mkdir -p Scripts.out/Twemoji
 cd Scripts.out/Twemoji
 
@@ -12,33 +14,24 @@ rm -rf twemoji
 wget https://nightly.link/SebastianAigner/twemoji-amazing/workflows/build_css/master/artifact.zip
 unzip artifact.zip
 rm artifact.zip
-mv twemoji-amazing.css twemoji-amazing-local.css
+mv twemoji-amazing.css $File
 
 # Replace spaces with tabs, save space
-sed -i 's/    /	/' twemoji-amazing-local.css
+sed -i 's/    /	/' $File
 # Blank out CDN URL
-sed -i 's/https:\/\/twemoji.maxcdn.com\/v\/latest\///' twemoji-amazing-local.css
+sed -i 's/https:\/\/twemoji.maxcdn.com\/v\/latest\///' $File
 
 # Personal CSS patches
-cat >> twemoji-amazing-local.css << [EOF]
+cat >> $File << [EOF]
 
 /* 
 	Personal CSS patches from
-	https://gitlab.com/octtspacc/postoctt
+	https://gitlab.com/octtspacc/sitoctt
 */
 
 .twa span {
-    /* Prevents image and text emoji overlap */
-    font-size: 0px;
+	/* Prevents image and text emoji overlap */
+	font-size: 0px;
 }
 
 [EOF]
-
-#wget -O "twemoji-amazing-local.css" "https://octtspacc.gitlab.io/sitoctt-assets/twemoji-amazing.css"
-#sed -i 's/https:\/\/twemoji.maxcdn.com\/v\/latest\///' twemoji-amazing-local.css
-
-#From="https://twemoji.maxcdn.com/v/latest/svg"
-#for i in $(grep "    background-image: url(" "twemoji-amazing-local.css" | cut -c30- | cut -c${#From}- | rev | cut -c3- | rev)
-#do
-#	wget -O "$i" "$From/$i"
-#done
