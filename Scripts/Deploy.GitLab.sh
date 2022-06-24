86470554d69cdb01509348c20889e46c9c8c09f2
octospacc <octo@tutamail.com>
2022-06-24 22:30:45 +0200

    Aggiornamento PicoBlog; Agg. interni

---
+++ Scripts/Deploy.GitLab.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python3 staticoso/Source/Build.py \
+	--SiteName="sitoctt" \
+	--SiteDomain="https://sitoctt.octt.eu.org" \
+	--SiteLang="it" \
+	--FolderRoots="{'*':'//octtspacc.gitlab.io'}" \
+	--ContextParts="{'Footer':['StatCounter.html']}"
--- Scripts/TestBuild.sh
+++ Scripts/TestBuild.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-../staticoso/Source/Build.py --FolderRoots="{'*':'//octtspacc.gitlab.io'}"
+../staticoso/Source/Build.py \
+	--SiteName="sitoctt" \
+	--SiteDomain="https://sitoctt.octt.eu.org" \
+	--SiteLang="it" \
+	--FolderRoots="{'*':'//octtspacc.gitlab.io'}"
