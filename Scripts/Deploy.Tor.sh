0c1943d036c74e416b0fa2b4107501cf5cd33e38
octospacc <octo@tutamail.com>
2022-06-28 13:53:31 +0200

    Agg. script interni

---
+++ Scripts/Deploy.Tor.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3 ../staticoso/Source/Build.py \
+		--SiteName="sitoctt" \
+		--SiteDomain="http://sitoctt2fxjvx3lc2iqqef2aeulflprjaasv2xl4zi7sxxbmvjy5b5yd.onion" \
+		--SiteLang="it" \
+		--SiteRoot="/" \
+		--FolderRoots="{'*':''}" \
+		--ContextParts="{'Header':'Tor.html'}"
