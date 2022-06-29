a51bc3a252e15aea39b65260cf059bd511542a10
octospacc <octo@tutamail.com>
2022-06-30 00:28:30 +0200

    Agg. PicoBlog; Nuovo indirizzo Onion

--- Scripts/Build.Test.sh
+++ Scripts/Build.Test.sh
@@ -4,4 +4,5 @@ python3 ../staticoso/Source/Build.py \
 	--SiteName="sitoctt" \
 	--SiteDomain="https://sitoctt.octt.eu.org" \
 	--SiteLang="it" \
-	--FolderRoots="{'*':'https://octtspacc.gitlab.io'}"
+	--FolderRoots="{'*':'https://octtspacc.gitlab.io'}" \
+	--GemtextOut=True
