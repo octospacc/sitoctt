f0123d6b27b2b4dab4f32128ddafa0def561058b
octospacc <octo@tutamail.com>
2022-07-01 18:08:00 +0200

    Test x4

--- Scripts/Deploy.Gemini.sh
+++ Scripts/Deploy.Gemini.sh
@@ -3,7 +3,7 @@
 git config --global user.email "$GitUserEmail"
 git config --global user.name "$GitUserName"
 git config --global credential.helper store
-echo "https://$GitUserName:$GitPassword@gitlab.com" > .git-credentials
+echo "https://$GitUserName:$GitPassword@gitlab.com" > ~/.git-credentials
 
 git clone --depth 1 https://gitlab.com/octtspacc/octt.gmi
 cd octt.gmi
