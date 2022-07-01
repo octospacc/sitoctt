2366d387525fbf3903cdab32ac24e5542343959a
octospacc <octo@tutamail.com>
2022-07-01 17:58:12 +0200

    Test x2

--- Scripts/Deploy.Gemini.sh
+++ Scripts/Deploy.Gemini.sh
@@ -4,6 +4,8 @@ git clone --depth 1 https://gitlab.com/octtspacc/octt.gmi
 cd octt.gmi
 mv ../public.gmi ./sitoctt
 
+git config user.email "$GitUserEmail"
+git config user.name "$GitUserName"
 git add .
 git commit -m "Agg. $(date)"
 git push
