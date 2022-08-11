8cb16c519daad9d2f46a5b5b0f22efabad73b3d9
octospacc <octo@tutamail.com>
2022-08-11 21:40:37 +0200

    Test BitTorrent

--- Scripts/Build.Termux.sh
+++ Scripts/Build.Termux.sh
@@ -6,4 +6,5 @@ python3 ../staticoso/Source/Build.py \
 	--SiteDomain="https://sitoctt.octt.eu.org" \
 	--FolderRoots="{'*':'https://octtspacc.gitlab.io'}" \
 	--Minify="False" \
+	--ContextParts="{'Head':'Privacy.html'}" \
 	--MarkdownExts="('attr_list', 'def_list', 'footnotes', 'md_in_html', 'tables')"
