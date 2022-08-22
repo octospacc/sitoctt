680cb643325d19b7a4f8faf952f5b4d970e7d351
octospacc <octo@tutamail.com>
2022-08-23 00:39:26 +0200

    Test fix build

--- Scripts/Prepare.GitLab.html2gmi.sh
+++ Scripts/Prepare.GitLab.html2gmi.sh
@@ -4,11 +4,14 @@ git clone --depth 1 https://github.com/LukeEmmet/html2gmi
 cd html2gmi
 go get github.com/LukeEmmet/html2gmi
 
-cat > ./html2gmi << [EOF]
-#!/bin/sh
-cd $PWD
-go run .
-[EOF]
+echo "#!/bin/sh" > ./html2gmi
+echo "cd $PWD; go run ." >> ./html2gmi
+
+#cat > ./html2gmi << [EOF]
+##!/bin/sh
+#cd $PWD
+#go run .
+#[EOF]
 
 chmod +x ./html2gmi
 PATH=$PATH:$PWD
