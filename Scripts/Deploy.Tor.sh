659c3bc4b804457226082312c8605db726f20d87
octospacc <octo@tutamail.com>
2022-11-22 23:47:28 +0100

    Agg CSS: Bottone mostra desktop; Agg. build script Tor

--- Scripts/Deploy.Tor.sh
+++ Scripts/Deploy.Tor.sh
@@ -4,3 +4,4 @@ python3 ../staticoso/Source/Build.py \
 	--SiteDomain="http://sitoctt2fxjvx3lc2iqqef2aeulflprjaasv2xl4zi7sxxbmvjy5b5yd.onion" \
 	--FolderRoots="{'*':'/', 'Assets':'/sitoctt-assets'}" \
 	--NoScripts=True
+./Scripts/Prepare.NoStyle.sh
